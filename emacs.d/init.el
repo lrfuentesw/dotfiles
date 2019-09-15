73dd412a46cfb59f59a4230b70a988974abc5351
Luis Raúl Fuentes Woolrich <luiswoolrich@gmail.com>
2019-09-14 20:33:47 -0500

    Evil deshabilitado

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,8 +20,8 @@
   (require 'use-package))
 ;; Ensure packages are installed
 ;; Evil mode
-(use-package evil
-  :ensure t)
+;(use-package evil
+;  :ensure t)
 ;; Spacemacs theme
 (use-package spacemacs-theme
   :no-require t
@@ -60,6 +60,18 @@
 ;; Magit
 (use-package magit
   :ensure t)
+;; Magit
+(use-package org-kanban
+  :ensure t)
+;; Elpy
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode))
 ;;
 ;;
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -68,12 +80,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    ["#d2ceda" "#f2241f" "#67b11d" "#b1951d" "#3a81c3" "#a31db1" "#21b8c7" "#655370"])
- '(custom-enabled-themes (quote (tsdh-light)))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (spacemacs-light)))
  '(custom-safe-themes
    (quote
-    ("1a1cdd9b407ceb299b73e4afd1b63d01bbf2e056ec47a9d95901f4198a0d2428" "30289fa8d502f71a392f40a0941a83842152a68c54ad69e0638ef52f04777a4c" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "59e82a683db7129c0142b4b5a35dbbeaf8e01a4b81588f8c163bd255b76f4d21" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
+    ("1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "a7051d761a713aaf5b893c90eaba27463c791cd75d7257d3a8e66b0c8c346e77" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "1a1cdd9b407ceb299b73e4afd1b63d01bbf2e056ec47a9d95901f4198a0d2428" "30289fa8d502f71a392f40a0941a83842152a68c54ad69e0638ef52f04777a4c" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "59e82a683db7129c0142b4b5a35dbbeaf8e01a4b81588f8c163bd255b76f4d21" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
+ '(fci-rule-color "#383838")
+ '(hl-paren-background-colors (quote ("#e8fce8" "#c1e7f8" "#f8e8e8")))
+ '(hl-paren-colors (quote ("#40883f" "#0287c8" "#b85c57")))
+ '(hl-sexp-background-color "#1c1f26")
  '(hl-todo-keyword-faces
    (quote
     (("TODO" . "#dc752f")
@@ -92,10 +112,39 @@
      ("XXX" . "#dc752f")
      ("XXXX" . "#dc752f")
      ("???" . "#dc752f"))))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (plan9-theme dracula-theme moe-theme material-theme cyberpunk-theme use-package evil spacemacs-theme)))
- '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef"))))
+    (flycheck elpy zenburn-theme gruvbox-theme plan9-theme dracula-theme moe-theme material-theme cyberpunk-theme use-package evil spacemacs-theme)))
+ '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef")))
+ '(sml/active-background-color "#98ece8")
+ '(sml/active-foreground-color "#424242")
+ '(sml/inactive-background-color "#4fa8a8")
+ '(sml/inactive-foreground-color "#424242")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -115,8 +164,17 @@
 (setq org-agenda-files (list "~/org/actividades.org"
 			     "~/org/OrgTutorial.org"))
 ;; Set evil mode
-(require 'evil)
-(evil-mode 1)
+;(setq evil-want-C-i-jump nil)
+;(require 'evil)
+;(evil-mode 1)
+
+;; Use Flycheck elpy
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;; YASnippet
+(require 'yasnippet)
+(yas-global-mode 1)
 
 (use-package heaven-and-hell
   :ensure t
@@ -127,3 +185,14 @@
   :hook (after-init . heaven-and-hell-init-hook)
   :bind (("C-c <f6>" . heaven-and-hell-load-default-theme)
          ("<f6>" . heaven-and-hell-toggle-theme)))
+
+;; Set space as indentation
+(progn
+  (setq-default indent-tabs-mode nil)
+ )
+
+;; MySQL
+(add-hook 'sql-interactive-mode-hook
+          (lambda ()
+            (toggle-truncate-lines t)))
+;;; init.el ends here
